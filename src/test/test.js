6b04aa6c7e9543a89ec113e24fdf2809694258fe
unknown <sushilkumar4sep@gmail.com>
2018-11-16 13:04:07 +0530

    added sample unit test case

--- src/test/test.js
+++
@@ -1,6 +0,0 @@
-import { getItemsForCart } from "../js/ShoppingCart/ShoppingCartService";
-const assert = require("assert");
-
-it("Succesfully got the response from json server", () => {
-  assert.equal(getItemsForCart(), 4);
-});
